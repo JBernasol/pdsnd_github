61cfca1ce351ca5d14826c115f4be4a546bf1811
Jamie Bernasol <jamie.bernasol@lmco.com>
2022-12-09 19:59:38 -0500

    Style: Added description to header of project scope

--- bikeshare.py
+++ bikeshare.py
@@ -1,7 +1,7 @@
 import time 
 import pandas as pd 
 import numpy as np 
-
+#below is coding for the bikeshare data for three cities 
 
 CITY_DATA = { 'chicago': 'chicago.csv', 
              'new york city': 'new_york_city.csv',
